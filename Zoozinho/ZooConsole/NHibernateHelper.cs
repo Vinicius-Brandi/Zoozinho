8dda94d1a4208efc0021744c170d26bd13fc2af9
Vinicius-Brandi <vviniciusbrandi@gmail.com>
2025-07-04 14:00:56 -0300

    deu algum problema serio na conexão do banco de dados

---
+++ Zoozinho/ZooConsole/NHibernateHelper.cs
@@ -0,0 +1,33 @@
+﻿using NHibernate;
+using NHibernate.Cfg;
+using System.Reflection;
+
+namespace ZooConsole
+{
+    public static class NHibernateHelper
+    {
+        private static ISessionFactory _sessionFactory;
+
+        public static ISessionFactory SessionFactory
+        {
+            get
+            {
+                if (_sessionFactory == null)
+                {
+                    var config = new Configuration();
+                    config.Configure("hibernate.cfg.xml");
+                    config.AddAssembly(Assembly.GetExecutingAssembly());
+
+                    _sessionFactory = config.BuildSessionFactory();
+                }
+
+                return _sessionFactory;
+            }
+        }
+
+        public static ISession AbrirSessao()
+        {
+            return SessionFactory.OpenSession();
+        }
+    }
+}
--- Zoozinho/ZooConsole/Program.cs
+++ Zoozinho/ZooConsole/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using ZooConsole.Models;
+using NHibernate;
+
+namespace ZooConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var categoria = new Categoria
+            {
+                Nome = "Testando NHibernate"
+            };
+
+            using (ISession session = NHibernateHelper.AbrirSessao())
+            using (ITransaction tx = session.BeginTransaction())
+            {
+                session.Save(categoria);
+                tx.Commit();
+            }
+
+            Console.WriteLine("Categoria salva com sucesso! ID: " + categoria.Id);
+        }
+    }
+}
